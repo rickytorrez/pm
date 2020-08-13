1639aee28aebb90e3e6d8ef79a72ac3c901484b5
rickytorrez <torrez.ricky@ymail.com>
2020-08-13 17:59:55 -0400

    project entity, h2 app props

---
+++ backend/src/main/java/com/ertorrez/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.ertorrez.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ertorrez.ppmtool.models.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+	
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}
