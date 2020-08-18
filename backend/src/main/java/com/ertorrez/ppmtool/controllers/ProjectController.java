0e99702f91c6990801fcb762fdcf73907101e33b
rickytorrez <torrez.ricky@ymail.com>
2020-08-18 12:32:42 -0400

    validation service

--- backend/src/main/java/com/ertorrez/ppmtool/controllers/ProjectController.java
+++ backend/src/main/java/com/ertorrez/ppmtool/controllers/ProjectController.java
@@ -9,13 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ertorrez.ppmtool.models.Project;
+import com.ertorrez.ppmtool.services.MapValidationErrorService;
 import com.ertorrez.ppmtool.services.ProjectService;
 
 @RestController
@@ -25,21 +25,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService _pS;
 	
+	@Autowired
+	private MapValidationErrorService _vS;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
-		if(result.hasErrors()) {
-			// extracting the field and defaultMessage from the error object
-			// key: field, value: message
-			
-			Map<String, String> errorMap =new HashMap<>();
-			
-			for(FieldError error: result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-				
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap = _vS.MapValidationService(result);
+		
+		if(errorMap != null) return errorMap;
 		
 		Project _project = _pS.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(_project, HttpStatus.CREATED);
