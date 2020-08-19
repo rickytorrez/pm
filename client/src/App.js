9110f0123dfdbaedab80cb21675394dce11fada0
rickytorrez <torrez.ricky@ymail.com>
2020-08-19 13:40:01 -0400

    redux store set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-
 import AddProject from './components/Project/AddProject';
+import store from './store/store';
 
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Route exact path='/dashboard' component={Dashboard} />
-        <Route exact path='/addProject' component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <Header />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/addProject' component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
