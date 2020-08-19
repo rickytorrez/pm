848dd01fcf6cd97b1f68fbba11f6a83cf1c3f50b
rickytorrez <torrez.ricky@ymail.com>
2020-08-19 11:21:36 -0400

    front end

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+
+import AddProject from './components/Project/AddProject';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Header />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/addProject' component={AddProject} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
