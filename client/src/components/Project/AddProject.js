85bc088e6ccd285a3e9358e1465f17631dbe1e41
rickytorrez <torrez.ricky@ymail.com>
2020-08-20 16:49:29 -0400

    lifecycle

--- client/src/components/Project/AddProject.js
+++ client/src/components/Project/AddProject.js
@@ -12,10 +12,17 @@ class AddProject extends Component {
       description: '',
       start_date: '',
       end_date: '',
+      errors: {},
     };
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onChange(e) {
@@ -35,6 +42,8 @@ class AddProject extends Component {
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div>
         <div className='register'>
@@ -53,6 +62,7 @@ class AddProject extends Component {
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectName}</p>
                   </div>
                   <div className='form-group'>
                     <input
@@ -63,6 +73,7 @@ class AddProject extends Component {
                       value={this.state.projectIdentifier}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectIdentifier}</p>
                   </div>
                   {/* <!-- disabled for Edit Only!! remove "disabled" for the Create operation --> */}
                   <div className='form-group'>
@@ -111,6 +122,11 @@ class AddProject extends Component {
 
 AddProject.propTypes = {
   createProject: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
-export default connect(null, { createProject })(AddProject);
+const mapStateToProps = (state) => ({
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { createProject })(AddProject);
